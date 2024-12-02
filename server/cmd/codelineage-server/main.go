6de58aaab2157e601a4999befcd7a640f2174afa
Pat Smuk <pat.smuk@indexexchange.com>
2024-12-02 11:19:16 -0500

    Server initial code

--- server/cmd/codelineage-server/main.go
+++ server/cmd/codelineage-server/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
 
+type RequestMessage struct {
+	RequestID int32       `json:"requestId"`
+	Type      string      `json:"type"`
+	Data      interface{} `json:"data"`
+}
+
+type TagsResponseMessage struct {
+	RequestID int32  `json:"requestId"`
+	TODO      string `json:"todo"`
+}
+
 func main() {
-	fmt.Println("testing")
+	// Receive JSON from stdin
+	decoder := json.NewDecoder(os.Stdin)
+	for {
+		var requestMsg RequestMessage
+		if err := decoder.Decode(&requestMsg); err != nil {
+			// Handle error
+			fmt.Printf("%s", err.Error())
+			os.Exit(1)
+		}
+
+		// Process message...
+
+		// Send response via stdout
+		encoder := json.NewEncoder(os.Stdout)
+		responseMsg := TagsResponseMessage{
+			RequestID: requestMsg.RequestID,
+			TODO:      "",
+		}
+		if err := encoder.Encode(responseMsg); err != nil {
+			// Handle error
+			fmt.Printf("%s", err.Error())
+			os.Exit(1)
+		}
+	}
 }
