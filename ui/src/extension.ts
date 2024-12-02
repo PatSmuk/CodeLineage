157df616ade0d505b4de788cf8f907e519da97b3
Pat Smuk <pat.smuk@indexexchange.com>
2024-12-02 12:01:21 -0500

    Update project

--- ui/src/extension.ts
+++ ui/src/extension.ts
@@ -1,26 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { spawn } from "child_process";
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "codelineage.analyzeGoCode",
+    () => {
+      // Get the root folder of the current workspace
+      const workspaceFolders = vscode.workspace.workspaceFolders;
+      if (!workspaceFolders || workspaceFolders.length === 0) {
+        vscode.window.showErrorMessage("No workspace folder found");
+        return;
+      }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "codelineage" is now active!');
+      const rootPath = workspaceFolders[0].uri.fsPath;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('codelineage.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeLineage!');
-	});
+      // Spawn the Go process
+      const process = spawn("codelineage-server", [rootPath], {
+        cwd: rootPath, // Optional: set working directory
+      });
 
-	context.subscriptions.push(disposable);
+      // Handle stdout
+      process.stdout.on("data", (data) => {
+        try {
+          const jsonData = JSON.parse(data.toString());
+          // Process the JSON response
+          console.log("Received:", jsonData);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      });
+
+      // Handle stderr
+      process.stderr.on("data", (data) => {
+        console.error(`Go process stderr: ${data}`);
+      });
+
+      // Handle process exit
+      process.on("close", (code) => {
+        console.log(`Go process exited with code ${code}`);
+      });
+
+      process.stdin.write('{"requestId": 99, "type": "FOO"}\n');
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
